85d1515e056bb87eef048c3968a8392da7244e87
Rahul Jaiswal <106617099+RAhpoo@users.noreply.github.com>
2022-10-26 13:24:52 +0530

    Add files via upload

---
+++ Core Java LabTest 26Oct/src/com/Employeestest/Test.java
@@ -0,0 +1,27 @@
+
+
+/*Employee Details 
+ * @Author:Rahul Jaiswal
+@Date: 26/10/22*/
+
+//creating packages
+package com.Employeestest;
+
+//importing employee package
+import com.Employeesdetails.Employees;
+
+//creating class
+public class Test {
+
+	//main method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//calling Get Details method
+		Employees.GetDetails();
+		
+		//calling Print Details method
+		Employees.PrintDetails();
+	}//end of main
+
+}//end of class
---
+++ Core Java LabTest 26Oct/src/com/employees/Employees.java
@@ -0,0 +1,40 @@
+/*
+Program: A student will not be allowed to sit in exam if his/her attendence is less than 75%.
+Take following input from user
+Number of classes held
+Number of classes attended.
+And print
+percentage of class attended
+Is student is allowed to sit in exam or not."
+@author: Rahul jaiswal
+@Date: 26  oct 2022  
+*/
+ 
+package com.employees;
+import java.util.*;
+//declaring a class to employees package
+//Declaring  class 
+class Employees
+{
+	//main method
+public static void main(String args[])
+{
+	
+ Scanner sc=new Scanner(System.in);
+ //print no of class held
+ System.out.println("Enter the No of classes held");
+ int x=sc.nextInt();
+ //print a no of class attend
+ System.out.println("Enter the No of classes attend");
+ int y=sc.nextInt();
+ //Declaring a data type and variable
+float pf;
+ pf=((y*100)/x);
+ System.out.println(pf);
+if(pf>=75)
+System.out.println("Student is allow to sit in the examS:"+ pf);
+else 
+System.out.println(" Not Eligible for a student is not allow to sit in the exam:"+ pf);
+}
+//end main method
+}
