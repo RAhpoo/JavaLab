eab71b1a262c7142848e5e266f5076ca8183c27c
DOORA <DOORA@LAPTOP-509EC8EE>
2022-12-29 20:18:57 +0530

    ok

---
+++ LabTest29December/src/com/ques3/StrReverse.java
@@ -0,0 +1,18 @@
+package com.ques3;
+
+
+public class StrReverse {
+	
+	public void strrev(String s)
+	{
+		int i;
+		String rev="";
+		int len = s.length();
+		for(i=len; i>0; --i)
+		{
+			rev = rev+ s.charAt(i-1);
+		}
+		System.out.println("Reverse of String is :"+rev);
+	}
+
+}
